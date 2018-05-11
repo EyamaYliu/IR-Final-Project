2addacc8b6cef0cb8de21f37c7d77fe462d4ef83
EyamaYliu <yliu53501@gmail.com>
2018-05-11 17:05:44 -0400

    Update

--- parser.py
+++ parser.py
@@ -14,24 +14,66 @@ def get_links(root, html):
         if title:
             ing_lines.append(str(title))
     return ing_lines
+
+def divider(lines,num):
+
+    pos = lines.index(num)
+    sum = 0
+    while pos != 0:
+        pos = pos - 1
+        try:
+            sum += float(lines[pos])
+        except:
+            continue
+    num = num.split('/')
+    return float(num[0])/float(num[1])+sum
         
 
-site = 'https://www.allrecipes.com/recipe/68532/curried-coconut-chicken/?internalSource=previously%20viewed&referringContentType=home%20page&clickId=cardslot%203'
+site = 'https://www.allrecipes.com/recipe/68532/curried-coconut-chicken/?internalSource=previously%20viewed&referringContentType=home%20page&clickId=cardslot%203'  
+
 r = request.urlopen(site)
 
 measurement = [line.rstrip('\n') for line in open('Measurements')]
 
 
-print(measurement)
 
+#print(measurement)
 
 ingredient_lines = get_links(site,r.read())
 
+
+ingredient_dict = {}
+
 for lines in ingredient_lines:
+    YesUnit = False
+    #Get rid of all parentheses
+    lines = re.sub('[(){}<>]','',lines)
     lines = lines.split(' ')
     for word in lines:
-        if word in measurement:
-            print(word)
-    print(lines)
+        for unit in measurement:
+            #Check if line contains standard unit
+            if unit in word:
+                portion = ' '.join(lines[lines.index(word)-1:lines.index(word)+1])
+                key = ' '.join(lines[lines.index(word)+1:])
+                ingredient_dict[key] = portion
+                YesUnit = True
+    if not YesUnit:
+        numpos = 0
+        for i in range(len(lines)):
+            if '/' in lines[i]:
+                lines[i] = divider(lines,lines[i])
+                numpos = i
+        portion = lines[numpos]
+        key = ' '.join(lines[numpos+1:])
+        ingredient_dict[key] = portion
+
+
+            
+                
+
+print(ingredient_dict)
+
+
+search_site = 'www.calorieking.com/food/search.php?keywords='
 
 
