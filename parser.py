6502092625d38ea1334d9b6d080d5bb863f47da7
Yujie Liu <36554196+EyamaYliu@users.noreply.github.com>
2018-05-15 21:10:44 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ import re
 from bs4 import BeautifulSoup
 from urllib import parse, request
 #import tldextract
+import nltk
+nltk.download('popular')
 
 def get_links(root, html):
 
@@ -27,9 +29,9 @@ def divider(lines,num):
             continue
     num = num.split('/')
     return float(num[0])/float(num[1])+sum
-        
 
-site = 'https://www.allrecipes.com/recipe/68532/curried-coconut-chicken/?internalSource=previously%20viewed&referringContentType=home%20page&clickId=cardslot%203'  
+
+site = 'https://www.allrecipes.com/recipe/68532/curried-coconut-chicken/?internalSource=previously%20viewed&referringContentType=home%20page&clickId=cardslot%203'
 
 r = request.urlopen(site)
 
@@ -55,7 +57,18 @@ for lines in ingredient_lines:
             if unit in word:
                 portion = ' '.join(lines[lines.index(word)-1:lines.index(word)+1])
                 key = ' '.join(lines[lines.index(word)+1:])
-                ingredient_dict[key] = portion
+                #print(key)
+
+                #FIND RELEVANT FOOD NOUNS IN STRING
+                is_noun = lambda pos: pos == 'NN'
+                # do the nlp stuff
+                tokenized = nltk.word_tokenize(key)
+                #only want nouns
+                nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if is_noun(pos)]
+                nouns = ' '.join(nouns)
+                #print(nouns)
+
+                ingredient_dict[nouns] = portion
                 YesUnit = True
     if not YesUnit:
         numpos = 0
@@ -65,15 +78,16 @@ for lines in ingredient_lines:
                 numpos = i
         portion = lines[numpos]
         key = ' '.join(lines[numpos+1:])
+
+
+
         ingredient_dict[key] = portion
 
 
-            
-                
+
+
 
 print(ingredient_dict)
 
 
 search_site = 'www.calorieking.com/food/search.php?keywords='
-
-
